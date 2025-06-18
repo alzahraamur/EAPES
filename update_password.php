2e845c8151f82bd6a98df607637ad449b5cf8c72
alzahraamur <alzahraamur11@gmail.com>
2025-06-19 01:29:35 +0400

    Update update_password.php

--- update_password.php
+++ update_password.php
@@ -1,13 +1,13 @@
 <?php
 include 'include/db_config.php';
 
-// كلمة المرور الجديدة (غير مشفّرة)
+
 $newPassword = "12341234Aa";
 
-// تشفيرها
+
 $hashedPassword = password_hash($newPassword, PASSWORD_DEFAULT);
 
-// تحديث المستخدم المطلوب
+
 $email = "admin@example.com";
 $stmt = $pdo->prepare("UPDATE users SET password = ? WHERE email = ?");
 $stmt->execute([$hashedPassword, $email]);
